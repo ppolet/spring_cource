d0bad7a0103a10291e3c435374d8132a1c1dd7df
pm.poletaev <ppolet@mail.ru>
2022-09-27 20:28:33 +0300

    Initial commit

---
+++ src/main/java/spring_introduction/Person.java
@@ -0,0 +1,56 @@
+package spring_introduction;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
+public class Person {
+
+    private Pet pet;
+//    @Value("${person.surname}")
+    private String surname;
+//    @Value("${person.age}")
+    private int age;
+
+    @Autowired
+    public Person(@Qualifier("catBean") Pet pet){
+        System.out.println("Person bean is created");
+        this.pet = pet;
+    }
+
+    public Person() {
+        System.out.println("Person bean is created");
+    }
+
+//    @Autowired
+//    @Qualifier("dog")
+    public void setPet(Pet pet) {
+        System.out.println("Class person: set pet");
+        this.pet = pet;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class person: set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class person: set AGE");
+        this.age = age;
+    }
+
+    public void callYourPet(){
+        System.out.println("Hello my pet!");
+        pet.say();
+    }
+}
